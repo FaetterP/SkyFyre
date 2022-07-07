4afff81bc8b2c7296e30fa40125ce3c7800f2758
FaetterP <56697273+FaetterP@users.noreply.github.com>
2022-07-07 13:48:25 +0300

    Добавлен скрипт персонажа

---
+++ Assets/Scripts/Player/Character.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Player
+{
+    class Character : MonoBehaviour
+    {
+        [SerializeField] private Transform _projectileSpawn;
+        [SerializeField] private PlayerProjectile _projectile;
+
+        private void Update()
+        {
+            Vector2 vec = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = vec;
+
+            if (Input.GetKey(KeyCode.Mouse0))
+            {
+                Instantiate(_projectile, _projectileSpawn.position, Quaternion.identity);
+            }
+        }
+    }
+}
