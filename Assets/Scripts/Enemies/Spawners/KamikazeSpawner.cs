7fe23e48996ee7f1c5d39a50ce131a4600d2dd57
FaetterP <56697273+FaetterP@users.noreply.github.com>
2022-07-11 23:16:49 +0300

    Добавлены новые волны
    Изменён KamikazeSpawner: теперь он призывает врагов на задающейся в инспекторе координате x.
    Соответственно, спавнеры также были перестроены под это изменение.

--- Assets/Scripts/Enemies/Spawners/KamikazeSpawner.cs
+++ Assets/Scripts/Enemies/Spawners/KamikazeSpawner.cs
@@ -5,11 +5,12 @@ namespace Assets.Scripts.Enemies.Spawners
 {
     class KamikazeSpawner : Spawner
     {
+        [SerializeField] private float _spawnX;
         private static System.Random rnd = new System.Random();
 
         public override void Spawn(Wave wave)
         {
-            Vector2 position = new Vector2(rnd.Next(1000, 1100), rnd.Next(-300, 300));
+            Vector2 position = new Vector2(_spawnX + rnd.Next(0, 100), rnd.Next(-300, 300));
             EnemyEndPoint spawned = Instantiate(_enemyEndPoint, position, Quaternion.identity);
             spawned.Init(wave);
         }
