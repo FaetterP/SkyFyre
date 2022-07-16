0da8686e2e261702a3c333b9204eeb77eee1464a
FaetterP <56697273+FaetterP@users.noreply.github.com>
2022-07-16 15:29:42 +0300

    Добавлен фон
    Фон движется с заданной скоростью. Цикличность достигается тем, что при достижении центра экрана появляется такой же фон справа. А когда текущий фон выходит из поля зрения камеры, он удаляется.

---
+++ Assets/Scripts/Stage/Background.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Stage
+{
+    class Background : MonoBehaviour
+    {
+        [SerializeField] private float _speed;
+        [SerializeField] private Transform _nextPoint;
+        private bool _isCreatedNext;
+
+        private void Awake()
+        {
+            _isCreatedNext = false;
+        }
+
+        private void Update()
+        {
+            transform.position -= new Vector3(Time.deltaTime * _speed, 0, 0);
+            if (transform.position.x < 0 && _isCreatedNext == false)
+            {
+                Instantiate(gameObject, _nextPoint.position , Quaternion.identity);
+                _isCreatedNext = true;
+            }
+        }
+
+        private void OnBecameInvisible()
+        {
+            if (transform.position.x < 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
