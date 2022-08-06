b2f8aefa2532a00feae6577b63b82d91108ea535
FaetterP <56697273+FaetterP@users.noreply.github.com>
2022-08-06 20:15:16 +0300

    Изменена сцена с выбором уровней
    Добавлены экраны "Character" и "Shop".

---
+++ Assets/Scripts/Gui/Menu/SwitcherCanvas.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Gui.Menu
+{
+    class SwitcherCanvas : MonoBehaviour
+    {
+        [SerializeField] private List<Canvas> _toEnable;
+        [SerializeField] private List<Canvas> _toDisable;
+
+        public void Switch()
+        {
+            foreach (Canvas canvas in _toDisable)
+            {
+                canvas.gameObject.SetActive(false);
+            }
+            foreach (Canvas canvas in _toEnable)
+            {
+                canvas.gameObject.SetActive(true);
+            }
+        }
+    }
+}
