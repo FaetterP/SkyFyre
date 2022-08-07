69ddbf8d8ff2284dc21f5f11923634a884c965d3
FaetterP <56697273+FaetterP@users.noreply.github.com>
2022-08-07 20:28:27 +0300

    Добавлен счётчик характеристик персонажа

---
+++ Assets/Scripts/Gui/Menu/Character/StatsCounter.cs
@@ -0,0 +1,29 @@
+﻿namespace Assets.Scripts.Gui.Menu.Character
+{
+    class StatsCounter
+    {
+        private int _value;
+        private static int s_points = 5;
+        public int Value => _value;
+        public static int Points => s_points;
+
+        public StatsCounter(int value)
+        {
+            _value = value;
+        }
+
+        public void AddValue()
+        {
+            if (s_points <= 0)
+                return;
+
+            s_points--;
+            _value++;
+        }
+
+        public static void AddPoints(int count)
+        {
+            s_points += count;
+        }
+    }
+}
---
+++ Assets/Scripts/Gui/Menu/Character/StatsHandler.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Gui.Menu.Character
+{
+    class StatsHandler : MonoBehaviour
+    {
+        [SerializeField] private Text _textValue;
+        [SerializeField] private Text _textPoints;
+        private StatsCounter _stat;
+
+        private void Awake()
+        {
+            _stat = new StatsCounter(5);
+        }
+
+        private void Start()
+        {
+            UpdateTexts();
+        }
+
+        public void AddStat()
+        {
+            _stat.AddValue();
+            UpdateTexts();
+        }
+
+        private void UpdateTexts()
+        {
+            _textValue.text = _stat.Value.ToString();
+            _textPoints.text = StatsCounter.Points.ToString();
+        }
+    }
+}
